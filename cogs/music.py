579d475f92adc8eae4ab7cdcf16dac8d42fa5197
HikuYaki2250 <84562555+KouFan2250@users.noreply.github.com>
2022-05-15 18:20:07 +0900

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -2,42 +2,18 @@ import asyncio
 import discord
 from discord.ext import commands
 import youtube_dl
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-}
-ffmpeg_options = {
-    'options': '-vn'
-}
-FFMPEG_BEFORE_OPTS = '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
 
-        self.data = data
+FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
-        self.title = data.get('title')
-        self.url = data.get('url')
+YDL_OPTIONS = = {
+            "format" : "bestaudio",
+            "postprocessors" : [{
+                "key" : "FFmpegExtractAudio",
+                "preferredcodec" : "mp3",
+                "preferredquality" : "192",
+            }], "noplaylist" : "True"
+        }
 
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 class music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -54,12 +30,23 @@ class music(commands.Cog):
     async def play(self,ctx,link):
         await ctx.send("お待ちください...")
         with ctx.message.channel.typing():
-            source = discord.PCMVolumeTransformer(
-            discord.FFmpegPCMAudio(link, before_options=FFMPEG_BEFORE_OPTS), volume=1.0)
+            with YoutubeDL(YDL_OPTIONS) as ydl:
+                try: 
+                    info = ydl.extract_info("ytsearch:%s" % link, download=False)['entries'][0]
+                except Exception: 
+                    pass
+
             
-            #player = await YTDLSource.from_url(link, loop=asyncio.get_event_loop())
-            ctx.message.author.guild.voice_client.play(source, after=lambda e: print(
-                'Player error: %s' % e) if e else None)
+            
+            
+            voice = get(bot.voice_clients, guild=ctx.guild)
+            
+            voice.play(discord.FFmpegPCMAudio(info['formats'][0]['url'], FFMPEG_OPTIONS))
+            voice.source = discord.PCMVolumeTransformer(voice.source)
+            voice.source.volume = 0.07
+            
+            
+         
             await ctx.message.channel.send(f'再生しています！')
     @commands.command(aliases=["resume"])
     async def pause(self, ctx):
